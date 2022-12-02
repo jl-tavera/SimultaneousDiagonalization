f9e867cacf712e817b6159a06eef519e54811add
jltavera <jl.tavera@uniandes.edu.co>
2022-12-02 01:27:13 -0500

    Functions update

--- functions.py
+++ functions.py
@@ -1,8 +1,14 @@
 import numpy as np 
 from scipy.stats import ortho_group
+from functools import partial
 from scipy.optimize import minimize
 
+
 def normal_matrix(n):
+    '''
+    By generating a random orthonormal matrix and two random diagonal
+    matrix creates two normal matrix that commute
+    '''
     Q = ortho_group.rvs(dim = n)
     Q_transpose = np.transpose(Q)
 
@@ -16,8 +22,6 @@ def normal_matrix(n):
     B = Q @ diag_B @ Q_transpose
 
     return A, B, Q
-
-normal_matrix = normal_matrix(4)
 
 
 def off_2(A, B):
@@ -67,7 +71,46 @@ def create_M_ij(A,B,i,j):
     
     return M
 
-def objective(M, x):
+def create_e_vector(n, pos):
+    e_vector = []
+    
+    for k in range(n):
+        entry = [0]
+        if k == pos:
+            entry = [1]
+       
+        e_vector.append(entry)
+    
+    e_vector = np.array(e_vector)
+    return e_vector
+
+def create_Rij(n, i, j, sols):
+    # sols = sols.x
+    identity = np.identity(n)
+
+    theta = sols[0]
+    phi = sols[1]
+
+    c = np.cos(theta)
+    c_c = np.conjugate(c)
+
+    s = np.exp(1j*phi)*np.sin(theta)
+    s_c = np.conjugate(s)
+
+    e_i = create_e_vector(n, i)
+    e_i_t = np.transpose(e_i) 
+
+    e_j = create_e_vector(n, j)
+    e_j_t = np.transpose(e_j) 
+
+    R = identity + (c - 1)*(e_i.dot(e_i_t)) - (s_c)*(e_i.dot(e_j_t)) + (s)*(e_j.dot(e_i_t)) + (c_c - 1)*(e_j.dot(e_j_t))
+
+    return R
+
+def objective_function(M, x):
+    '''
+    Objective Function
+    '''
     theta = x[0]
     phi = x[1]
 
@@ -79,6 +122,9 @@ def objective(M, x):
     return np.linalg.norm(M.dot(z))
 
 def constraint(x):
+    '''
+    Constraint
+    '''
     theta = x[0]
     phi = x[1]
 
@@ -88,47 +134,67 @@ def constraint(x):
     return np.square(abs(c)) + np.square(abs(s)) - 1
 
 def bnds():
+    '''
+    Bounds
+    '''
     b_1 = (-1*np.pi/4, np.pi/4)
     b_2 = (-1*np.pi, np.pi)
 
     bounds = (b_1, b_2)
 
     return bounds
-
-
-def minimization(M):
-   
-
     return None
 
-
+np.set_printoptions(suppress=True, precision=4)
     
-def SimultaneousDiag(normal_matrix, epsilon):
-    A = normal_matrix[0]
-    B = normal_matrix[1]
+epsilon = 0.01
+matrix = normal_matrix(3)
+print(matrix[2])
+x_0 = (-1*np.pi/4, -1*np.pi)
 
-    n = A.shape[0]
-    Q = np.identity(n)
-
-    A_norm = np.linalg.norm(A)
-    B_norm = np.linalg.norm(B)
-
-    off_two = off_2(A, B)
-    epsilon_norm = epsilon*(A_norm + B_norm)
-
-    while off_two > epsilon_norm:
-        for i in range(0, n):
-            for j in range(i+1, n):
-                M = create_M_ij(A,B,i,j)
-                min_problem = minimization(M)
-
-                
-                
+A = matrix[0]
+B = matrix[1]
 
 
-                
-    
+n = A.shape[0]
+Q = np.identity(n)
 
-    return None
+A_norm = np.linalg.norm(A)
+B_norm = np.linalg.norm(B)
 
-SimultaneousDiag(normal_matrix, 0.01)
+off_two = off_2(A, B)
+epsilon_norm = epsilon*(A_norm + B_norm)
+
+bounds = bnds()
+constraints = {'type':'eq', 'fun':constraint}
+cons = [constraints]
+
+while off_two > epsilon_norm:
+    for i in range(0, n):
+        for j in range(i+1, n):
+            M = create_M_ij(A,B,i,j)
+            objective = partial(objective_function, M)
+            sols = minimize(objective,x_0, method='SLSQP', bounds=bounds, constraints=cons)
+            R = create_Rij(n, i, j, [np.pi/10, np.pi/10])
+            R = np.asmatrix(R)
+            R_H = R.H
+
+            Q = Q @ R
+            Q_transpose = np.transpose(Q)
+
+
+            A = R_H @A@R
+            B = R_H @B@R
+
+            diag_A = Q_transpose @ A @ Q
+            print(sols.x)
+            print(diag_A)
+
+            A_norm = np.linalg.norm(A)
+            B_norm = np.linalg.norm(B)
+
+            off_two = off_2(A, B)
+            print(off_two)
+            epsilon_norm = epsilon*(A_norm + B_norm)
+
+
